397f06855a3b826d741a9c9186e716d73d80008a
wanyixuan_i <wanyixuan_i@didiglobal.com>
2021-01-09 20:54:30 +0800

    uodate tokenizer

--- tiny-compiler.js
+++ tiny-compiler.js
@@ -1,5 +1,49 @@
 function tokenizer (input) {
+    let current = 0
 
+    let tokens = []
+
+    while(current < input.length) {
+        let char = input[current]
+
+        if(char === '('){
+            tokens.push({
+                type: 'paren',
+                value: '('
+            })
+            current++
+        continue
+        }
+        
+        if(char===')'){
+            tokens.push({
+                type: 'paren',
+                value: ')'
+            })
+        }
+        continue
+
+        let WHITESPACE = /\s/
+        if(WHITESPACE.test(char)){
+            current++
+            continue
+        }
+
+        let NUMBERS = /[0-9]/
+        if(NUMBERS.test(char)){
+            let value = ''
+
+            while(NUMBERS.test(char)){
+                value+ = char
+                char = input[++current]
+            }
+            tokens.push({
+                type: Number,
+                value
+            })
+            continue
+        }
+    }
 }
 
 function parser(tokens){
