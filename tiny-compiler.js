bc2ead22818f2fe876a8cd54fc5002e41d8c99bb
wanyixuan_i <wanyixuan_i@didiglobal.com>
2021-01-11 23:56:09 +0800

    update

--- tiny-compiler.js
+++ tiny-compiler.js
@@ -3,19 +3,19 @@ function tokenizer (input) {
 
     let tokens = []
 
-    while(current < input.length) {
+    while (current < input.length) {
         let char = input[current]
 
-        if(char === '('){
+        if (char === '(') {
             tokens.push({
                 type: 'paren',
                 value: '('
             })
             current++
-        continue
+            continue
         }
-        
-        if(char===')'){
+
+        if (char === ')') {
             tokens.push({
                 type: 'paren',
                 value: ')'
@@ -24,17 +24,17 @@ function tokenizer (input) {
         continue
 
         let WHITESPACE = /\s/
-        if(WHITESPACE.test(char)){
+        if (WHITESPACE.test(char)) {
             current++
             continue
         }
 
         let NUMBERS = /[0-9]/
-        if(NUMBERS.test(char)){
+        if (NUMBERS.test(char)) {
             let value = ''
 
-            while(NUMBERS.test(char)){
-                value+ = char
+            while (NUMBERS.test(char)) {
+                value + = char
                 char = input[++current]
             }
             tokens.push({
@@ -43,28 +43,43 @@ function tokenizer (input) {
             })
             continue
         }
+
+        let LETTERS = /[a-z]/i
+        if (LETTERS.test(char)) {
+            let value = ''
+            while (LETTERS.test(char)) {
+                value += char
+                char = input[++current]
+            }
+            tokens.push({
+                type: 'name',
+                value
+            })
+            continue
+        }
+
         throw new TypeError('I dont know what this character is' + char)
     }
     return tokens
 }
 
-function parser(tokens){
+function parser (tokens) {
 
 }
 
-function traverser(ast, visitor){
+function traverser (ast, visitor) {
 
 }
 
-function transformer(ast){
+function transformer (ast) {
 
 }
 
-function codeGenerator(node){
+function codeGenerator (node) {
 
 }
 
-function compiler(input){
+function compiler (input) {
     let tokens = tokenizer(input)
     let ast = parser(tokens)
     let newAst = transformer(ast)
