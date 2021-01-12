37e157ad942bcd9cbf8fc1dfba99e252183896a2
wanyixuan_i <wanyixuan_i@didiglobal.com>
2021-01-12 15:03:47 +0800

    add traverse

--- tiny-compiler.js
+++ tiny-compiler.js
@@ -130,7 +130,40 @@ function parser (tokens) {
 }
 
 function traverser (ast, visitor) {
+    function traverseArray(array, parent) {
+        // 遍历AST节点
+        Array.forEach(child => {
+            traverseNode(child, parent)
+        })
+    }
 
+    function traverseNode(node, parent) {
+        let methods = visitor[node.type]
+        if(methods && methods.enter) {
+            methods.enter(node, parent)
+        }
+
+        switch(node.type) {
+            case 'Program':
+                traverseArray(node.body, node)
+                break;
+
+            case 'CallExpression':
+                traverseArray(node.params ,node)
+                break;
+            case 'NumberLiteral':
+            case 'StringLiteral':
+                break;
+
+            default:
+                throw new TypeError(node.type)
+        }
+
+        if(methods && methods.exit) {
+            methods.exit(node, parent)
+        }
+    }
+    traverseNode(ast, null)
 }
 
 function transformer (ast) {
